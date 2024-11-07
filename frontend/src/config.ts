d40c1150f679c559ede5dead93c4171328d0b0f2
Daniel Kaga <daniel.kaga@gmail.com>
2024-11-06 22:33:22 -0800

    refactor(frontend-config): adds default values for env vars

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,8 +1,8 @@
 import { z } from 'zod'
 
 const configSchema = z.object({
-  VITE_API_URL: z.string().url(),
-  VITE_BASE_URL: z.string()
+  VITE_API_URL: z.string().url().default('http://localhost:3000'),
+  VITE_BASE_URL: z.string().default('/')
 })
 
 export const config = configSchema.parse(import.meta.env)
