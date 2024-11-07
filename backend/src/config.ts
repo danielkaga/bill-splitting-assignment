2ff6e255d1ec5915eddca4c51d7359d412a5ba55
Daniel Kaga <daniel.kaga@gmail.com>
2024-11-06 17:01:28 -0800

    feat(security): adds cors policy
    Adds cors allowed origins from env var.

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -3,8 +3,14 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
-const configSchema = z.object({
-  JWT_SECRET: z.string()
-})
+const configSchema = z
+  .object({
+    JWT_SECRET: z.string(),
+    CORS_ALLOWED_ORIGINS: z.string()
+  })
+  .transform((obj) => ({
+    ...obj,
+    CORS_ALLOWED_ORIGINS: obj.CORS_ALLOWED_ORIGINS.split(',')
+  }))
 
 export const config = configSchema.parse(process.env)
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,21 @@
 import express from 'express'
 import authRoutes from '@src/routes/auth'
 import accountsRoutes from '@src/routes/accounts'
+import cors from 'cors'
+import { config } from './config'
+
+const { CORS_ALLOWED_ORIGINS } = config
 
 const app = express()
 const port = 3000
 
 app.use(express.json())
+app.use(
+  cors({
+    origin: CORS_ALLOWED_ORIGINS,
+    credentials: true
+  })
+)
 
 app.use('/auth', authRoutes)
 
