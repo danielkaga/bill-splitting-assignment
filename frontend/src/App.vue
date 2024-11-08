64a217eebf3847df4a0ade740498137e0dc8af48
Daniel Kaga <daniel.kaga@gmail.com>
2024-11-08 14:49:32 -0800

    style(global): adds global styling for mobile view

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -5,3 +5,19 @@ import { RouterView } from 'vue-router'
 <template>
   <RouterView />
 </template>
+
+<style>
+html,
+body {
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+}
+
+.parent-container {
+  position: relative;
+  overflow: hidden;
+  width: 100vw;
+  height: 100vh;
+}
+</style>
